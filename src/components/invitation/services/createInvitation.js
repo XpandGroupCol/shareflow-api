e79644091cefee1742952b056be1258582d68555
DiegoContrerasA <diegocontreras1219@gmail.com>
2022-07-26 06:45:17 -0500

    email is removed

--- src/components/invitation/services/createInvitation.js
+++ src/components/invitation/services/createInvitation.js
@@ -1,26 +1,12 @@
 const { createLead } = require('../../../libraries/activeCampaign')
 const Invitation = require('../../../models/Invitation')
-const { welcomeTemplate } = require('../../../templates/welcome')
-const { sendEmail } = require('../../../utils/aws/SES')
 
 const createInvitation = async ({ name, email, phone, lastName }) => {
   const user = await Invitation.findOne({ email }).lean().exec()
   if (user) return true
   const activeCampaignId = await createLead({ firstName: name, email, phone, lastName })
-  await Invitation.create({ name, email, phone, lastName, activeCampaignId })
-
-  const sendEmailPayload = {
-    destinationEmails: ['diegocontreras1219@gmail.com'],
-    emailSubject: 'Bienvenido a Shareflow',
-    text: 'Bienvenido a Shareflow',
-    htmlMessage: welcomeTemplate({ name })
-  }
-  try {
-    const data = await sendEmail(sendEmailPayload)
-    return data
-  } catch (e) {
-    return false
-  }
+  const data = await Invitation.create({ name, email, phone, lastName, activeCampaignId })
+  return data
 }
 
 module.exports = createInvitation
