a20eb140447666c2d5cb544ade7e65975d656e60
DiegoContrerasA <diegocontreras1219@gmail.com>
2022-09-06 10:28:45 -0500

    fix publishers

--- src/components/publisher/services/getPublisherById.js
+++ src/components/publisher/services/getPublisherById.js
@@ -1,12 +1,13 @@
 const Publisher = require('../../../models/Publisher')
 const boom = require('@hapi/boom')
 
-const clearPublishers = ({ _id, sex, ageRange, category, publisher, formats, logo, miniBudget }) => ({
+const clearPublishers = ({ _id, sex, ageRange, category, publisher, formats, logo, miniBudget, locations }) => ({
   _id: _id,
   publisher,
   logo,
   miniBudget,
   sex,
+  locations: locations.map(({ city, _id, department }) => ({ label: `${city}, ${department}`, value: _id })),
   ageRange: ageRange.map(({ _id, name }) => ({ value: _id, label: name })),
   category,
   formats: formats.map(({ format, target, pricePerUnit, biddingModel, device }) => ({
@@ -23,6 +24,7 @@ const getPublisherById = async (id) => {
   const data = await Publisher.findById(id).populate('ageRange')
     .populate('formats.format')
     .populate('formats.target')
+    .populate('locations')
     .lean().exec()
   if (!data) throw boom.badRequest('Usuario no encontrado')
   return clearPublishers(data)
