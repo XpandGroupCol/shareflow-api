68764fa1e7adf1ee432eae6e2b7bf97a2adffd37
DiegoContrerasA <diegocontreras1219@gmail.com>
2023-03-15 16:28:17 -0500

    add invitation

--- src/components/invitation/services/createInvitation.js
+++ src/components/invitation/services/createInvitation.js
@@ -1,12 +1,14 @@
 const { createLead } = require('../../../libraries/activeCampaign')
 const Invitation = require('../../../models/Invitation')
+const sendInvitationEmail = require('./sendInvitationEmail')
 
 const createInvitation = async ({ name, email, phone, lastName }) => {
   const user = await Invitation.findOne({ email }).lean().exec()
   if (user) return true
   const activeCampaignId = await createLead({ firstName: name, email, phone, lastName })
   const data = await Invitation.create({ name, email, phone, lastName, activeCampaignId })
-  return data
+  const sendEmail = await sendInvitationEmail(data?._id)
+  return { ...data, sendEmail }
 }
 
 module.exports = createInvitation
